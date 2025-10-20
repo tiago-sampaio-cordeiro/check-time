8e17a161a5a0e4617b56bef5214dfa3f4f0d7197
Tiago Sampaio Cordeiro <145937944+tiago-sampaio-cordeiro@users.noreply.github.com>
2025-10-20 18:59:56 -0300

    changed stack navigation to tabs navigation (#14)

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,43 @@
 import React from "react";
-import { Stack } from "expo-router";
+import { Stack, Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 
 export default function _layout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="calculateHours" options={{ headerShown: false }} />
-      <Stack.Screen name="listTime" options={{ headerShown: false }} />
-    </Stack>
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <Ionicons name="home-outline" color={"#501be0ff"} size={20} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="calculateHours"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <MaterialIcons name="calculate" size={24} color="#501be0ff" />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="listTime"
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <MaterialCommunityIcons
+              name="clipboard-list-outline"
+              size={24}
+              color="#501be0ff"
+            />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
