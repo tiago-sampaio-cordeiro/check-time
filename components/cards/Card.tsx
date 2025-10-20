651fe236d25b69c0ecdae7803dbbb89671f19ab6
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-10-20 16:06:25 -0300

    created card component

---
+++ components/cards/Card.tsx
@@ -0,0 +1,60 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export interface CardProps {
+  cardTitle: string;
+  horarioEntrada: string;
+  horarioSaida: string;
+  horasTotal: string;
+}
+
+export default function Card({
+  cardTitle,
+  horarioEntrada,
+  horarioSaida,
+  horasTotal,
+}: CardProps) {
+  return (
+    <View style={styles.containerCard}>
+      <View>
+        <Text style={styles.titleCard}>Data: {cardTitle}</Text>
+      </View>
+      <View style={styles.registrosETotal}>
+        <View style={styles.registrosOnly}>
+          <View>
+            <Text>Entrada: {horarioEntrada}</Text>
+          </View>
+          <View>
+            <Text>Entrada: {horarioSaida}</Text>
+          </View>
+        </View>
+        <View>
+          <Text>Total: {horasTotal}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  containerCard: {
+    width: 350,
+    backgroundColor: "#fff",
+    height: 80,
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 10,
+  },
+  titleCard: {
+    textAlign: "center",
+  },
+  registrosETotal: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 20,
+  },
+  registrosOnly: {
+    flexDirection: "row",
+    gap: 30,
+  },
+});
