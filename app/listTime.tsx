2c1b04c2f24fb7b53a700b21816315ae72337241
Tiago Sampaio Cordeiro <sampaio415@gmail.com>
2025-11-24 16:43:34 -0300

    cards exibindo informações de cada registro

--- app/listTime.tsx
+++ app/listTime.tsx
@@ -1,12 +1,47 @@
 import { View, StyleSheet, ScrollView } from "react-native";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import TimeIcon from "../components/icons/TimeIcon";
 import TitleApp from "../components/title/TitleApp";
 import Subtitle from "../components/subtitle/Subtitle";
-import { times } from "../mocks/MockTimes";
-import Card from "../components/cards/Card";
+import Card, { CardProps } from "../components/cards/Card";
+import { pb } from "../services/pb";
+import { useFocusEffect } from "expo-router";
+
+interface Registro {
+  id: string;
+  data: string;
+  entrada: string;
+  saida: string;
+  total: string;
+}
 
 export default function listTime() {
+  const [registros, setRegistros] = useState<Registro[]>([]);
+
+  const loadRegistros = async () => {
+    try {
+      const data = await pb.collection("horas").getFullList({});
+
+      const registrosFormatados: Registro[] = data.map((r) => ({
+        id: r.id,
+        data: r.data,
+        entrada: r.entrada,
+        saida: r.saida,
+        total: r.total,
+      }));
+
+      setRegistros(registrosFormatados);
+    } catch (error) {
+      console.log("Erro ao buscar registros:", error);
+    }
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      loadRegistros();
+    }, [])
+  );
+
   return (
     <ScrollView>
       <View style={styles.container}>
@@ -14,15 +49,15 @@ export default function listTime() {
         <TitleApp />
         <Subtitle subtitleName="Registros" />
 
-        {times.map((item, index) => (
-          <Card
-            key={index}
-            cardTitle={item.cardTitle}
-            horarioEntrada={item.horarioEntrada}
-            horarioSaida={item.horarioSaida}
-            horasTotal={item.horasTotal}
-          />
-        ))}
+        {registros.map((r) => {
+          const cardProps: CardProps = {
+            cardTitle: r.data,
+            horarioEntrada: r.entrada,
+            horarioSaida: r.saida,
+            horasTotal: r.total,
+          };
+          return <Card key={r.id} {...cardProps} />;
+        })}
       </View>
     </ScrollView>
   );
